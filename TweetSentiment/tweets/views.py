dc80adad4cbfa7a70b7ede4e81474009b6c428de
Carlton Brady <Ctombrady@gmail.com>
2017-12-15 00:02:32 -0500

    Implemented tweet searching, sentiment classification, and display in a table on the UI.

--- TweetSentiment/tweets/views.py
+++ TweetSentiment/tweets/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
+from .forms import SearchForm
+import tweetSearch as tweetSearch
+import tweetClassify as tweetClassify
+
 
 
 # Create your views here.
 
 def index(request):
-    return render(request,'tweets/index.html')
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        
+        if form.is_valid():
+            if len(str(form.cleaned_data)) > 0:
+                searchTerm = str(form.cleaned_data['query'])
+                #pass the classified tweets to the page for populating the table
+                tweets = tweetSearch.getTweets(100,searchTerm)
+                tweetSentimentPairs = tweetClassify.classifyTweets(tweets,searchTerm)
+                sentimentStats = tweetClassify.computeSentimentStats(tweetSentimentPairs)
+                percent_neg = sentimentStats[0]
+                percent_pos = sentimentStats[1]
+                loadResults = True
+
+            
+    else:
+        form = SearchForm()
+        tweets = []
+        tweetSentimentPairs = []
+        sentimentStats = ["N/A","N/A"]
+        loadResults = False
+    
+    return render(request,'tweets/index.html',{'form':form,'tweets':tweets, 'pairs':tweetSentimentPairs, 'percent_neg':percent_neg,'percent_pos':percent_pos, 'loadResults':loadResults})
 
 def home(request):
     context = {
