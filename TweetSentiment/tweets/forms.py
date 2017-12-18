0a76a1acf92f576bcad628331381614ceaf5ab04
Carlton Brady <Ctombrady@gmail.com>
2017-12-17 23:04:12 -0500

    Added free text entry form

--- TweetSentiment/tweets/forms.py
+++ TweetSentiment/tweets/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(label ='Search for tweets containing:', max_length=70)
+    
+class ClassifyForm(forms.Form):
+    text = forms.CharField(label = 'Enter some text to classify:',max_length=300, widget=forms.Textarea)
